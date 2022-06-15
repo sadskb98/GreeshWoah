890ae89b9ddfa882cc85299263239d6a6178ae45
Shadman Sakib <sadskb98@.com>
2022-06-15 11:07:28 +0600

    15-06-11.06

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/"> GreeshWoah</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div classname="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,43 @@
+const data = {
+  products: [
+    {
+      name: 'Floral Print Empire Dress',
+      slug: 'floral-print-empire-dress',
+      category: 'Dresses',
+      image: '/images/Dress1.jpg',
+      price: 2399,
+      countInStock: 100,
+      brand: 'Brand 1',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality Fabric. Perfect as summer outfit.',
+    },
+
+    {
+      name: 'Purple Crepe Midi Dress',
+      slug: 'purple-crepe-midi-dress',
+      category: 'Dresses',
+      image: '/images/Dress2.jpg',
+      price: 2799,
+      countInStock: 75,
+      brand: 'Brand 2',
+      rating: 4.7,
+      numReviews: 17,
+      description: 'High Quality Fabric. Perfect for parties.',
+    },
+
+    {
+      name: 'Black NEO-Cool Cotton Sleeveless Lounge T-shirt',
+      slug: 'black-neo-cool-cotton-sleeveless-lounge-t-shirt',
+      category: 'Dresses',
+      image: '/images/TankTop.jpg',
+      price: 799,
+      countInStock: 100,
+      brand: 'Brand 3',
+      rating: 4.2,
+      numReviews: 21,
+      description: 'High Quality Fabric.',
+    },
+  ],
+};
+export default data;
