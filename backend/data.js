2c3e01bbc052e4a53ec82a9c64e2ce59e358a4b4
Shadman Sakib <sadskb98@.com>
2022-06-18 05:24:54 +0600

    18-06-05.24

---
+++ backend/data.js
@@ -0,0 +1,43 @@
+const data = {
+  products: [
+    {
+      name: 'Floral Print Empire Dress',
+      slug: 'floral-print-empire-dress',
+      category: 'Dresses',
+      image: '/images/p1.jpg', //360px * 480px
+      price: 2399,
+      countInStock: 100,
+      brand: 'Brand 1',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality Fabric. Perfect as summer outfit.',
+    },
+
+    {
+      name: 'Purple Crepe Midi Dress',
+      slug: 'purple-crepe-midi-dress',
+      category: 'Dresses',
+      image: '/images/p2.jpg',
+      price: 2799,
+      countInStock: 75,
+      brand: 'Brand 2',
+      rating: 4.7,
+      numReviews: 17,
+      description: 'High Quality Fabric. Perfect for parties.',
+    },
+
+    {
+      name: 'Black Tank Top',
+      slug: 'black-tank-top',
+      category: 'Dresses',
+      image: '/images/p3.jpg',
+      price: 799,
+      countInStock: 100,
+      brand: 'Brand 3',
+      rating: 4.2,
+      numReviews: 21,
+      description: 'High Quality Fabric.',
+    },
+  ],
+};
+export default data;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,23 @@
-import data from './data';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screen/HomeScreen';
+import ProductScreen from './screen/ProductScreen';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/"> GreeshWoah</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>৳{product.price}</strong>
-                </p>
-                <button> Add to Cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/"> GreeshWoah</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
